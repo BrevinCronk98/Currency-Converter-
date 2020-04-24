fa271f17439b312b3fa28435655fbc944b1b4c97
brevin <cronkbrevin98@gmail.com>
2020-04-24 09:44:17 -0700

    NPM webpacks installed and template setup up, website begin

---
+++ __tests__/money.test.js
@@ -0,0 +1,13 @@
+// import {Object } from '../src/money.js';
+
+describe('money', () => {
+	var money;
+
+	beforeEach(() => {
+		money = new Triangle(5, 5, 5);
+	});
+
+	test('your test', () => {
+		expect('this').toEqual('that');
+	});
+});
