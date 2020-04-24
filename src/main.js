b70c8966f919d8100699848ef02c0247adf41e2f
brevin <cronkbrevin98@gmail.com>
2020-04-24 11:51:25 -0700

    work on function to show conversion

--- src/main.js
+++ src/main.js
@@ -2,31 +2,32 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from 'jquery';
-import { Currency } from './money-api-fetch';
+// import { Currency } from './money-api-fetch';
+
+function showCurrrencies(curOne, curTwo) {
+	$('#curOneDisp').text('This is your first currency' + curOne);
+	$('#curTwoDisp').text('this is your 2nd currency' + curTwo);
+}
 
 $(document).ready(function() {
 	$('#submit-form').submit(function(event) {
 		event.preventDefault();
-		$('#conv-disp').text(' ');
+		$('#curOne').text(' ');
+		$('#curTwo').text(' ');
 
 		let curOne = $('#curOne').val();
+		let curTwo = $('#curTwo').val();
 
-		$('#curOne').val('');
-
+		$('#curOneDisp').val('');
+		$('#curTwoDisp').val(' ');
+		showCurrrencies(curOne, curTwo);
 		console.log(curOne);
-
-		(async () => {
-			let currency = new Currency();
-			const response = await currency.getCurrency(curOne);
-			getElementsForCur(response);
-		})();
-
-		function getElementsForCur(response) {
-			if (response) {
-				$('#conv-disp').text(`This is the conversion ${response.result}`);
-			} else {
-				$('#conv-disp').text(`Oh no there was an error`);
-			}
-		}
+		$('#submit-form').hide();
 	});
 });
+// (async () => {
+// 	let currency = new Currency();
+// 	const response = await currency.getCurrency(curOne);
+// 	const responseTwo = await currency.getCurrency(curTwo);
+// 	getElementsForCurOne(response);
+// })();
