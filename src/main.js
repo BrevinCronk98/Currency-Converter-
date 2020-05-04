9b16a71f2b31c1159241b45315ae9b77d9a68d82
brevin <cronkbrevin98@gmail.com>
2020-05-03 18:45:14 -0700

    final commit, api key is stored in my env file, fixed indentation warnings

--- src/main.js
+++ src/main.js
@@ -3,124 +3,123 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from 'jquery';
 import { Currency } from './money-api-fetch';
-require('dotenv').config();
 
 
 $(document).ready(function() {
   $('#number-form').submit(function(event) {
-		event.preventDefault();
-	$('#btn-row').show();
-	$('#conv-disp').hide();
+    event.preventDefault();
+    $('#btn-row').show();
+    $('#conv-disp').hide();
 
-	let num = $('#num-input').val();
-	displayUserNum(num);
-	function displayUserNum(num) {
-		$('#num-display').text(`You want to convert  ${num}  Dollars. `);
-	}
+    let num = $('#num-input').val();
+    displayUserNum(num);
+    function displayUserNum(num) {
+      $('#num-display').text(`You want to convert  ${num}  Dollars. `);
+    }
 
-	$('#EUR').click(function(event) {
-		event.preventDefault();
-		(async () => {
-			let currency = new Currency();
-			const response = await currency.getCurrency();
-			convEUR(num, response);
-			$('#conv-disp').show();
-		})();
+    $('#EUR').click(function(event) {
+      event.preventDefault();
+      (async () => {
+        let currency = new Currency();
+        const response = await currency.getCurrency();
+        convEUR(num, response);
+        $('#conv-disp').show();
+      })();
 
-		function convEUR(num, response) {
-			if (response) {
-				let convEUR;
-				convEUR = parseFloat(num * response.conversion_rates.EUR).toFixed(2);
-				$('#conv-disp').text(`The conversion to Euros is:  ` + convEUR);
-				return convEUR;
-			} else {
-				$('conv-disp').text('Oh no there was an error!');
-			}
-		}
-	});
+      function convEUR(num, response) {
+        if (response) {
+          let convEUR;
+          convEUR = parseFloat(num * response.conversion_rates.EUR).toFixed(2);
+          $('#conv-disp').text(`The conversion to Euros is:  ` + convEUR);
+          return convEUR;
+        } else {
+          $('conv-disp').text('Oh no there was an error!');
+        }
+      }
+    });
 
-	$('#JPY').click(function(event) {
-		event.preventDefault();
-			(async () => {
-				let currency = new Currency();
-				const response = await currency.getCurrency();
-				convJPY(num, response);
-				$('#conv-disp').show();
-			})();
+    $('#JPY').click(function(event) {
+      event.preventDefault();
+      (async () => {
+        let currency = new Currency();
+        const response = await currency.getCurrency();
+        convJPY(num, response);
+        $('#conv-disp').show();
+      })();
 
-			function convJPY(num, response) {
-				if (response) {
-					let convJPY;
-					convJPY = parseFloat(num * response.conversion_rates.JPY).toFixed(2);
-					$('#conv-disp').text(`The conversion  to Japanese Yen is:  ` + convJPY);
-					return convJPY;
-				} else {
-					$('conv-disp').text('Oh no there was an error!');
-				}
-			}
-		});
+      function convJPY(num, response) {
+        if (response) {
+          let convJPY;
+          convJPY = parseFloat(num * response.conversion_rates.JPY).toFixed(2);
+          $('#conv-disp').text(`The conversion  to Japanese Yen is:  ` + convJPY);
+          return convJPY;
+        } else {
+          $('conv-disp').text('Oh no there was an error!');
+        }
+      }
+    });
 
-		$('#RUB').click(function(event) {
-			event.preventDefault();
-			(async () => {
-				let currency = new Currency();
-				const response = await currency.getCurrency();
-				convRUB(num, response);
-				$('#conv-disp').show();
-			})();
+    $('#RUB').click(function(event) {
+      event.preventDefault();
+      (async () => {
+        let currency = new Currency();
+        const response = await currency.getCurrency();
+        convRUB(num, response);
+        $('#conv-disp').show();
+      })();
+ 
+      function convRUB(num, response) {
+        if (response) {
+          let convRUB;
+          convRUB = parseFloat(num * response.conversion_rates.RUB).toFixed(2);
+          $('#conv-disp').text(`The conversion to Russian Rubles is:  ` + convRUB);
+          return convRUB;
+        } else {
+          $('conv-disp').text('Oh no there was an error!');
+        }
+      }
+    });
 
-			function convRUB(num, response) {
-				if (response) {
-					let convRUB;
-					convRUB = parseFloat(num * response.conversion_rates.RUB).toFixed(2);
-					$('#conv-disp').text(`The conversion to Russian Rubles is:  ` + convRUB);
-					return convRUB;
-				} else {
-					$('conv-disp').text('Oh no there was an error!');
-				}
-			}
-		});
+    $('#FJD').click(function(event) {
+      event.preventDefault();
+      (async () => {
+        let currency = new Currency();
+        const response = await currency.getCurrency();
+        convFJD(num, response);
+        $('#conv-disp').show();
+      })();
 
-		$('#FJD').click(function(event) {
-			event.preventDefault();
-			(async () => {
-				let currency = new Currency();
-				const response = await currency.getCurrency();
-				convFJD(num, response);
-				$('#conv-disp').show();
-			})();
+      function convFJD(num, response) {
+        if (response) {
+          let convFJD;
+          convFJD = parseFloat(num * response.conversion_rates.FJD).toFixed(2);
+          $('#conv-disp').text(`The conversion to Fijian Dollars is:  ` + convFJD);
+          return convFJD;
+        } else {
+          $('conv-disp').text('Oh no there was an error!');
+        }
+      }
+    });
 
-			function convFJD(num, response) {
-				if (response) {
-					let convFJD;
-					convFJD = parseFloat(num * response.conversion_rates.FJD).toFixed(2);
-					$('#conv-disp').text(`The conversion to Fijian Dollars is:  ` + convFJD);
-					return convFJD;
-				} else {
-					$('conv-disp').text('Oh no there was an error!');
-				}
-			}
-		});
+    $('#HKD').click(function(event) {
+      event.preventDefault();
+      (async () => {
+        let currency = new Currency();
+        const response = await currency.getCurrency();
+        convHKD(num, response);
+        $('#conv-disp').show();
+      })();
 
-		$('#HKD').click(function(event) {
-			event.preventDefault();
-			(async () => {
-				let currency = new Currency();
-				const response = await currency.getCurrency();
-				convHKD(num, response);
-				$('#conv-disp').show();
-			})();
-
-			function convHKD(num, response) {
-				if (response) {
-					let convHKD;
-					convHKD = parseFloat(num * response.conversion_rates.HKD).toFixed(2);
-					$('#conv-disp').text(`The conversion to Hong Kong Dollars is:  ` + convHKD);
-					return convHKD;
-				} else {
-					$('conv-disp').text('Oh no there was an error!');
-				}
-			}
-		});
-	});
+      function convHKD(num, response) {
+        if (response) {
+          let convHKD;
+          convHKD = parseFloat(num * response.conversion_rates.HKD).toFixed(2);
+          $('#conv-disp').text(`The conversion to Hong Kong Dollars is:  ` + convHKD);
+          return convHKD;
+        } else {
+          $('conv-disp').text('Oh no there was an error!');
+        }
+      }
+    });
+  });
 });
--- src/money-api-fetch.js
+++ src/money-api-fetch.js
@@ -1,20 +1,17 @@
 
-require('dotenv').config();
-
 export class Currency {
-	async getCurrency() {
-		try {
-			let response = await fetch(`https://prime.exchangerate-api.com/v5/${process.env.API_KEY}/latest/USD`);
-			let jsonifiedResponse;
-			if (response.ok && response.status == 200) {
-				jsonifiedResponse = await response.json();
-			} else {
-				jsonifiedResponse = false;
-			}
-			return jsonifiedResponse;
-		} catch (error) {
-			// console.log(error);
-			return false;
-		}
-	}
+  async getCurrency() {
+    try {
+      let response = await fetch(`https://prime.exchangerate-api.com/v5/${process.env.API_KEY}/latest/USD`);
+      let jsonifiedResponse;
+      if (response.ok && response.status == 200) {
+        jsonifiedResponse = await response.json();
+      } else {
+        jsonifiedResponse = false;
+      }
+      return jsonifiedResponse;
+    } catch (error) {
+      return false;
+    }
+  }
 }
